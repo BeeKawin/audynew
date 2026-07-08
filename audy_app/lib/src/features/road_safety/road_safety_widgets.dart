5c209500c86fe3445d624820ed65aaf43bf71003
Pummiphach <pummiphach@hotmail.com>
2026-07-09 02:57:54 +0700

    Revert "Cross walk"
    This reverts commit a0a0658e6d2d6d00d512f9e208420e445a50d1bd.

--- audy_app/lib/src/features/road_safety/road_safety_widgets.dart
+++ audy_app/lib/src/features/road_safety/road_safety_widgets.dart
@@ -51,11 +51,9 @@ class RoadScene extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    final sceneHeight = adaptive.isPhone ? 300.0 : 360.0;
-
     return Container(
       width: double.infinity,
-      height: sceneHeight,
+      constraints: BoxConstraints(minHeight: adaptive.isPhone ? 300 : 360),
       padding: EdgeInsets.all(adaptive.space(18)),
       decoration: BoxDecoration(
         color: const Color(0xFFEAF7F6),
